b2eece0da8104e782d742b8916225c5a15cbf7f2
Matthias König <matthias.k.koenig@gmail.com>
2022-01-21 12:27:14 +0100

    vector samplers

--- include/random.h
+++ include/random.h
@@ -3,6 +3,7 @@
 #include "glm.hpp"
 #include "gtc/random.hpp"
 #include <random>
+#include <array>
 #include <chrono>
 #include "vecn.h"
 
@@ -44,6 +45,27 @@ namespace RNG
 		virtual T next() = 0;
 	};
 
+	template <typename T>
+	class NormalSampler : public Sampler<T>
+	{
+	protected:
+		unsigned int seed;
+		default_random_engine generator;
+		normal_distribution<float> normalDistribution;
+
+	public:
+		void init(unsigned int _count)
+		{
+			this->count = _count;
+			this->idx = 0;
+			seed = chrono::system_clock::now().time_since_epoch().count();
+			generator.seed(seed);
+			normalDistribution = normal_distribution<float>(0.0f, 1.0f);
+		}
+
+		virtual T next() = 0;
+	};
+
 	class UniformSampler1D : public UniformSampler<float>
 	{
 	public:
@@ -63,18 +85,75 @@ namespace RNG
 			return (idx++ + sample) / count;
 		}
 	};
-	
-	template<size_t N> 
-	class UniformSamplerND : public UniformSampler<vecn<float, N>>
+
+	class NormalSampler1D : public NormalSampler<float>
 	{
 	public:
+		float next(){
+			float sample = this->normalDistribution(this->generator);
+			return sample;
+		}
+	};
+
+	template<size_t N> 
+	class UniformSamplerND : public Sampler<vecn<float, N>>
+	{
+	protected:
+		array<UniformSampler1D, N> samplers;
+	public:
+		void init(unsigned int _count){
+			this->idx = 0;
+			this->count = _count;
+			for(auto &sampler: samplers){
+				sampler.init(_count);
+			}
+		}
+
 		vecn<float, N> next()
 		{
 			vecn<float, N> sampleVec;
 			for(size_t i = 0; i < N; ++i){
-				sampleVec[i] = this->uniformDistribution(this->generator);
+				sampleVec[i] = samplers[i].next();
 			}
 			return sampleVec;
+		}
+	};
+
+	template<size_t N> 
+	class NormalSamplerND : public Sampler<vecn<float, N>>
+	{
+	protected:
+		array<NormalSampler1D, N> samplers;
+	public:
+		void init(unsigned int _count){
+			this->idx = 0;
+			this->count = _count;
+			for(auto &sampler: samplers){
+				sampler.init(_count);
+			}
+		}
+
+		vecn<float, N> next()
+		{
+			vecn<float, N> sampleVec;
+			for(size_t i = 0; i < N; ++i){
+				sampleVec[i] = samplers[i].next();
+			}
+			return sampleVec;
+		}
+	};
+
+	template<size_t N>
+	class UniformBallSampler : public Sampler<vecn<float,N>>
+	{
+	public:
+		vecn<float, N> next()
+		{
+			vecn<float, N> u = UniformSamplerND<N>::next();
+			float norm = length(u);
+			float r = pow(this->uniformDistribution(this->generator), 1.0f/N);
+
+			return (u * r)/norm;
 		}
 	};
 } // namespace RNG
